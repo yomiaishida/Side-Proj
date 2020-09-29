2c8edd11e98f2bdb5a4ed614fde4a9126f5c5a7d
Abayomi Aishida <yomicorperate@gmail.com>
2020-09-29 20:20:57 +0100

    Traversing the dom to select preffered seats

--- Cinema Seats/script.js
+++ Cinema Seats/script.js
@@ -2,13 +2,13 @@ const movie = document.getElementById("movie");
 const container = document.querySelector(".container");
 let count = document.querySelector(".count");
 let total = document.querySelector(".total");
-const allOccupied = document
+const allPrefferred = document
   .querySelector(".container")
-  .getElementsByClassName("seat occupied");
+  .getElementsByClassName("seat preffered");
 
 const initialTotal = () => {
-  let initTotal = allOccupied.length * movie.value;
-  count.textContent = allOccupied.length;
+  let initTotal = allPrefferred.length * movie.value;
+  count.textContent = allPrefferred.length;
   total.textContent = initTotal;
   return;
 };
@@ -17,10 +17,12 @@ initialTotal();
 
 const addSeats = (e) => {
   let cls = e.target.classList;
-  if (cls.contains("seat")) {
-    cls.toggle("occupied");
-    count.textContent = allOccupied.length;
-    total.textContent = parseInt(allOccupied.length) * parseInt(movie.value);
+  if (cls.contains("occupied")) {
+    alert("Seat is already taken");
+  } else {
+    cls.toggle("preffered");
+    count.textContent = allPrefferred.length;
+    total.textContent = parseInt(allPrefferred.length) * parseInt(movie.value);
   }
 };
 
