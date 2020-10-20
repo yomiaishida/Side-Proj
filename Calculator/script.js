ea50445670bd029cbed814648d50d78f621a658a
Abayomi Aishida <yomicorperate@gmail.com>
2020-10-20 19:18:02 +0100

    Added functionality to add numbers

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,29 +1,48 @@
-let input = document.getElementById('input');
-let container = document.querySelector('.container')
+const input = document.getElementById('input');
+let container = document.querySelector('.container');
+const addBtn = document.querySelector('.add');
+let equalBtn = document.querySelector('.equal');
 
-console.log(input);
+let a = [];
 
-// numbers = Array.from(numbers)
-// console.log(numbers);
-
-input.addEventListener('onChange', (() => {
-  console.log(input.textContent);
-}))
-
-container.addEventListener('click', ((e)=> {
+const inputNum = (e) => {
+  
   let cls = e.target.classList;
   let content = e.target.textContent;
   // console.log(typeof(content));
 
   if(cls.contains('number')) {
-    console.log(e.target.textContent);
     input.value += (content);
-    // if(cls.contains('add')) {
-    //   input.value += content
-    // } else {
-
-    // }
-  } else {
-
   }
-}))
+}
+
+
+const addNum = () => {
+ a.push(input.value)
+
+ console.log(a);
+
+ input.value = ''
+}
+
+function totalNum() {
+  let total;
+  if(input.value !== '') {
+    a.push(input.value)
+    console.log(a);
+
+    a.forEach(num => {
+      total = parseFloat(num) +parseFloat(num);
+      console.log(total);
+    });
+    input.value = total
+  } else {
+    alert('Please input a number to be added')
+  }
+}
+
+container.addEventListener('click', inputNum)
+
+addBtn.addEventListener('click', addNum)
+
+equalBtn.addEventListener('click', totalNum)
